bf08b84f977e99e83d63f395b1f1c750273930dc
Yago B. Fontoura <yagofontoura222@yahoo.com>
2021-09-29 18:41:23 -0300

    Create Middleware and Tags Controllers

--- src/route.ts
+++ src/route.ts
@@ -1,12 +1,16 @@
 const express = require('express');
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController"
+import { ensureAdmin } from './middleware/ensureAdmin';
+
 
 
 const router = express.Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle);
-
+router.post("/tags", ensureAdmin ,createTagController.handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,32 @@
 import 'reflect-metadata'; 
-import express from 'express';
-import "./database";
+import  express, { Request, Response, NextFunction } from 'express';
+require("express-async-errors");
 import { router } from  "./route";
+
 import bp from 'body-parser';
+import "./database";
 
 const app = express();
 const port = 3000;
 
-app.use
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
 app.use(router);
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message,
+        })
+    }
+
+    return response.status(500).json({
+        status:"error",
+        message: "Internal Server Error"
+    })
+})
 
 
 
